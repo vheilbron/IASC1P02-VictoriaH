664bd874a9faecabd7967585ca031336b2fdb142
Victoria <vh18rz@brocku.ca>
2019-11-08 21:09:25 -0500

    Module 8

---
+++ Module8Lab2.js
@@ -0,0 +1,7 @@
+function textMagic() {
+
+	document.getElementById("blue").style.color="blue";
+	document.getElementById("green").style.color="green";
+	document.getElementById("red").style.color="red";
+
+}
